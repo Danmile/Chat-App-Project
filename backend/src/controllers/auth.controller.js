0baaf3f00ffed2e468c6b3127c86961cf86d8d40
Dan <danmil511@gmail.com>
2025-02-19 13:02:14 -0600

    Created user auth and started message endpoints

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,17 +1,103 @@
-export const signup =
-  ("/signup",
-  (req, res) => {
-    res.send("signup route");
-  });
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
-export const login =
-  ("/login",
-  (req, res) => {
-    res.send("login route");
-  });
+export const signup = async (req, res) => {
+  try {
+    const { fullname, email, password } = req.body;
+    if (!fullname || !email || !password) {
+      res.status(400).send("All fields are required!");
+    }
+    if (password.length < 6) {
+      res.status(400).send("Password must be at least 6 characters");
+    }
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).send("Email already exists");
 
-export const logout =
-  ("/logout",
-  (req, res) => {
-    res.send("logout route");
-  });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullname,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      // Generate jwt
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).send(newUser);
+    } else {
+      res.status(400).send("Invalid User data");
+    }
+  } catch (error) {
+    res.status(500).send("Server Error");
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).send("Invalid credentials");
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).send("Invalid credentials");
+    }
+    generateToken(user._id, res);
+
+    res.status(200).send(user);
+  } catch (error) {
+    console.log("Error in login", error.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+export const logout = (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).send("Logged out successfully");
+  } catch (error) {
+    console.log("Error in logout", error.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).send("Profile picture is required");
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true }
+    );
+
+    res.status(200).send(updatedUser);
+  } catch (error) {
+    console.log("Error in updateUser", error.message);
+    res.status(500).send("Server Error");
+  }
+};
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).send(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth", error.message);
+    res.status(500).send("Server Error");
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 dotenv.config();
 
+app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
