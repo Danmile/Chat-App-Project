1954cd583d4723775802a35cb0afbb87f4ee3ce1
Dan <danmil511@gmail.com>
2025-02-20 12:39:51 -0600

    Fixed backend bug in sendmessages, installed cors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
-import LoginPage from "./components/LoginPage";
-import SignupPage from "./components/SignupPage";
-import SettingsPage from "./components/SettingsPage";
-import ProfilePage from "./components/ProfilePage";
-import HomePage from "./components/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import HomePage from "./pages/HomePage";
 import { Routes, Route } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
 
 const App = () => {
+  const { authUser, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
   return (
     <div>
       <Navbar />
