375330e73b4a3a68cfea590c95ab4a49fd7a9186
Dan <danmil511@gmail.com>
2025-03-07 15:22:58 -0600

    Project Completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,8 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
-const BASE_URL = "http://localhost:5001";
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/";
 
 import { io } from "socket.io-client";
 export const useAuthStore = create((set, get) => ({
